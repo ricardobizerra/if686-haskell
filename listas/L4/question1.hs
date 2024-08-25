dc0d3145d2b4fe10ffd21d6ee35dd9954f6f893a
ricardobizerra <ricardo.bizerra19@gmail.com>
2024-08-25 17:26:18 -0300

    fix: remove consumer and produced elements go directly to the assembler

--- listas/L4/question1.hs
+++ listas/L4/question1.hs
@@ -12,26 +12,19 @@ main = do
   producedNutsQueue <- newMVar []
   producedBoltsQueue <- newMVar []
 
-  consumedNutsQueue <- newMVar []
-  consumedBoltsQueue <- newMVar []
-
   boxes <- newMVar []
   
   controlMVar <- newMVar True
 
-  forkIO $ producer producedNutsQueue controlMVar Nut -- máquina de produção de porcas
-  forkIO $ consumer producedNutsQueue consumedNutsQueue controlMVar Nut -- máquina de retirada de porcas
+  forkIO $ producer producedNutsQueue controlMVar Nut 2000000 -- máquina de produção de porcas
+  forkIO $ producer producedBoltsQueue controlMVar Bolt 1500000 -- máquina de produção de parafusos
+  forkIO $ boxAssembler boxes producedNutsQueue producedBoltsQueue controlMVar 1200000 -- montador de pares de porcas e parafusos
 
-  forkIO $ producer producedBoltsQueue controlMVar Bolt -- máquina de produção de parafusos
-  forkIO $ consumer producedBoltsQueue consumedBoltsQueue controlMVar Bolt -- máquina de retirada de parafusos
-
-  forkIO $ boxAssembler boxes consumedNutsQueue consumedBoltsQueue controlMVar -- montador de pares de porcas e parafusos
-
-producer :: MVar [Element] -> MVar Bool -> ElementType -> IO()
-producer queue control producerType = loop 1
+producer :: MVar [Element] -> MVar Bool -> ElementType -> Int -> IO()
+producer queue control producerType productionTime = loop 1
   where
     loop x = do
-      threadDelay 1383333
+      threadDelay productionTime
       initialQueue <- takeMVar queue
       let newElement = Element producerType x
       putMVar queue (initialQueue ++ [newElement])
@@ -40,32 +33,11 @@ producer queue control producerType = loop 1
       putMVar control ctrl
       loop (x + 1)
 
-consumer :: MVar [Element] -> MVar [Element] -> MVar Bool -> ElementType -> IO()
-consumer producedQueue consumedQueue control consumerType = loop
-  where
-    loop = do
-      threadDelay 1000000
-      produced <- takeMVar producedQueue
-      if null produced then do
-        ctrl <- takeMVar control
-        printf "Consumidor esperando por %s\n" (show consumerType)
-        putMVar control ctrl
-        putMVar producedQueue produced
-        loop
-      else do
-        ctrl <- takeMVar control
-        consumed <- takeMVar consumedQueue
-        let (producedHead:producedRest) = produced
-        putMVar consumedQueue (consumed ++ [producedHead])
-        printf "Consumidor consumiu %s %s\n" (show consumerType) (show (getElementId producedHead))
-        putMVar producedQueue producedRest
-        putMVar control ctrl
-        loop
-
-boxAssembler :: MVar [Box] -> MVar [Element] -> MVar [Element] -> MVar Bool -> IO()
-boxAssembler boxes consumedNuts consumedBolts control = loop 1
+boxAssembler :: MVar [Box] -> MVar [Element] -> MVar [Element] -> MVar Bool -> Int -> IO()
+boxAssembler boxes consumedNuts consumedBolts control assembleTime = loop 1
   where
     loop x = do
+      threadDelay assembleTime
       nuts <- takeMVar consumedNuts
       bolts <- takeMVar consumedBolts
       if null nuts || null bolts then do
@@ -87,11 +59,3 @@ boxAssembler boxes consumedNuts consumedBolts control = loop 1
         putMVar consumedNuts nutsRest
         putMVar consumedBolts boltsRest
         loop (x + 1)
-
--- funções auxiliares
-
-getElementType :: Element -> ElementType
-getElementType (Element elementType _) = elementType
-
-getElementId :: Element -> ElementId
-getElementId (Element _ id) = id
